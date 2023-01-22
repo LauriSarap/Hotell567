255c5b48f84b1e54c9b40f102c6a3515b12a949e
lauri <lauri.aardla@gmail.com>
2023-01-22 22:17:47 +0200

    Die ersten commiten

---
+++ MVVM/LoginPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace Hotell567.MVVM;
+
+public partial class LoginPage : ContentPage
+{
+	public LoginPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MVVM/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿namespace Hotell567.MVVM;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+        TypewriterEffect();
+    }
+
+    private async void OnCounterClicked(object sender, EventArgs e)
+    {
+        await Shell.Current.GoToAsync("LoginPage");
+        //Shell.Current.FlyoutIsPresented = true;
+    }
+
+    string text;
+    int lenght = 0;
+
+    private async void TypewriterEffect()
+    {
+        text = lblText.Text;
+        lblText.Text = "";
+
+        await Task.Delay(TimeSpan.FromSeconds(1));
+
+        for (int i = 0; i < text.Length; i++)
+        {
+            lblText.Text = lblText.Text + text.ElementAt(lenght);
+            lenght++;
+            await Task.Delay(TimeSpan.FromMilliseconds(100));
+        }
+    }
+}
+
