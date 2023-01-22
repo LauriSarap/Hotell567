92baf5777124df9d390e4d004e8e067282d45769
lauri <lauri.aardla@gmail.com>
2023-01-23 00:31:01 +0200

    Deleted shell and started adding a custom navbar

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace Hotell567;
+﻿using Hotell567.MVVM;
+
+namespace Hotell567;
 
 public partial class App : Application
 {
@@ -6,6 +8,6 @@ public partial class App : Application
 	{
 		InitializeComponent();
 
-		MainPage = new AppShell();
-	}
+		MainPage = new MainPage();
+    }
 }
--- MVVM/MainPage.xaml.cs
+++ MVVM/MainPage.xaml.cs
@@ -1,4 +1,7 @@
-﻿namespace Hotell567.MVVM;
+﻿using Windows.Networking.NetworkOperators;
+using Windows.UI.ApplicationSettings;
+
+namespace Hotell567.MVVM;
 
 public partial class MainPage : ContentPage
 {
@@ -8,25 +11,51 @@ public partial class MainPage : ContentPage
         TypewriterEffect();
     }
 
-    private async void OnCounterClicked(object sender, EventArgs e)
+    private void HomeBtn_Clicked(object sender, EventArgs e)
     {
-        await Shell.Current.GoToAsync("LoginPage");
-        //Shell.Current.FlyoutIsPresented = true;
+        NavigateToPage("Home");
     }
 
-    string text;
-    int lenght = 0;
+    private void LoginBtn_Clicked(object sender, EventArgs e)
+    {
+        NavigateToPage("LoginPage");
+    }
+
+    private void NavigateToPage(string page)
+    {
+        switch (page)
+        {
+            case "Home":
+                var homePage = new NavigationPage(new MainPage());
+                Application.Current.MainPage = homePage;
+                break;
+            case "LoginPage":
+                Application.Current.MainPage = new NavigationPage(new LoginPage());
+                break;
+            default:
+                break;
+        }
+    }
+
+
+    private void SeeAvailableListings(object sender, EventArgs e)
+    {
+        DisplayAlert("See Available Listings", "This feature is not yet implemented", "OK");
+    }
+
+    private string text;
+    private int lenght = 0;
 
     private async void TypewriterEffect()
     {
-        text = lblText.Text;
-        lblText.Text = "";
+        text = WelcomeLabel.Text;
+        WelcomeLabel.Text = "";
 
         await Task.Delay(TimeSpan.FromSeconds(1));
 
         for (int i = 0; i < text.Length; i++)
         {
-            lblText.Text = lblText.Text + text.ElementAt(lenght);
+            WelcomeLabel.Text = WelcomeLabel.Text + text.ElementAt(lenght);
             lenght++;
             await Task.Delay(TimeSpan.FromMilliseconds(100));
         }
