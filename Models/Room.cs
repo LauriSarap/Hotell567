9d543e1be58eec84649ff66419152f327f2ff57c
siimkaljuorg <siimkaljuorg@gmail.com>
2023-01-24 21:25:28 +0200

    Small

--- Models/Room.cs
+++ Models/Room.cs
@@ -7,5 +7,6 @@ public class Room
     public int RoomNumber { get; set; }
     public string Image { get; set; }
     public string Description { get; set; }
+    public bool IsAvailable { get; set; }
 }
 
