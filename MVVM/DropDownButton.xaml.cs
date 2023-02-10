7e7fc4c2b12057cba2e8e6c5255d487fa7b41578
Lauri Sarap <lauri.aardla@gmail.com>
2023-02-10 11:04:38 +0200

    Started adding info page

--- MVVM/DropDownButton.xaml.cs
+++
@@ -1,9 +0,0 @@
-namespace Hotell567.MVVM;
-
-public partial class DropDownButton : ContentView
-{
-	public DropDownButton()
-	{
-		InitializeComponent();
-	}
-}
---
+++ MVVM/InformationPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace Hotell567.MVVM;
+
+public partial class InformationPage : ContentPage
+{
+	public InformationPage()
+	{
+		InitializeComponent();
+	}
+}
